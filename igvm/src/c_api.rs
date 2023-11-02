a4f0fe04e00d2f50a5a427b4677bd4e29d2bcde2
Roy Hopkins <rhopkins@suse.de>
2023-11-02 12:51:53 +0000

    c_api: Add rust unit tests for C API functions
    Add unit tests that cover initialisation and destruction of the IGVM
    file using the C API and the querying of header and file data within the
    parsed file.
    
    Signed-off-by: Roy Hopkins <rhopkins@suse.de>

--- igvm/src/c_api.rs
+++ igvm/src/c_api.rs
@@ -490,3 +490,391 @@ pub extern "C" fn igvm_get_header_data(
         .or_else(|e| Ok(e.0) as Result<IgvmHandle, i32>)
         .unwrap()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::igvm_get_header;
+    use super::igvm_get_header_data;
+    use super::igvm_new_from_binary;
+    use crate::c_api::igvm_free;
+    use crate::c_api::igvm_free_buffer;
+    use crate::c_api::igvm_get_buffer;
+    use crate::c_api::igvm_get_buffer_size;
+    use crate::c_api::igvm_header_count;
+    use crate::c_api::IgvmHeaderSection;
+    use crate::c_api::IgvmResult;
+    use crate::IgvmDirectiveHeader;
+    use crate::IgvmFile;
+    use crate::IgvmInitializationHeader;
+    use crate::IgvmPlatformHeader;
+    use crate::IgvmRevision;
+    use igvm_defs::IgvmPageDataFlags;
+    use igvm_defs::IgvmPageDataType;
+    use igvm_defs::IgvmPlatformType;
+    use igvm_defs::IgvmVariableHeaderType;
+    use igvm_defs::IGVM_VHS_GUEST_POLICY;
+    use igvm_defs::IGVM_VHS_PAGE_DATA;
+    use igvm_defs::IGVM_VHS_PARAMETER;
+    use igvm_defs::IGVM_VHS_PARAMETER_AREA;
+    use igvm_defs::IGVM_VHS_PARAMETER_INSERT;
+    use igvm_defs::IGVM_VHS_SUPPORTED_PLATFORM;
+    use igvm_defs::IGVM_VHS_VARIABLE_HEADER;
+    use igvm_defs::PAGE_SIZE_4K;
+    use std::mem::size_of;
+
+    fn new_platform(
+        compatibility_mask: u32,
+        platform_type: IgvmPlatformType,
+    ) -> IgvmPlatformHeader {
+        IgvmPlatformHeader::SupportedPlatform(IGVM_VHS_SUPPORTED_PLATFORM {
+            compatibility_mask,
+            highest_vtl: 0,
+            platform_type,
+            platform_version: 1,
+            shared_gpa_boundary: 0,
+        })
+    }
+
+    fn new_guest_policy(policy: u64, compatibility_mask: u32) -> IgvmInitializationHeader {
+        IgvmInitializationHeader::GuestPolicy {
+            policy,
+            compatibility_mask,
+        }
+    }
+
+    fn new_page_data(page: u64, compatibility_mask: u32, data: &[u8]) -> IgvmDirectiveHeader {
+        IgvmDirectiveHeader::PageData {
+            gpa: page * PAGE_SIZE_4K,
+            compatibility_mask,
+            flags: IgvmPageDataFlags::new(),
+            data_type: IgvmPageDataType::NORMAL,
+            data: data.to_vec(),
+        }
+    }
+
+    fn new_parameter_area(index: u32) -> IgvmDirectiveHeader {
+        IgvmDirectiveHeader::ParameterArea {
+            number_of_bytes: 4096,
+            parameter_area_index: index,
+            initial_data: vec![],
+        }
+    }
+
+    fn new_parameter_usage(index: u32) -> IgvmDirectiveHeader {
+        IgvmDirectiveHeader::VpCount(IGVM_VHS_PARAMETER {
+            parameter_area_index: index,
+            byte_offset: 0,
+        })
+    }
+
+    fn new_parameter_insert(page: u64, index: u32, mask: u32) -> IgvmDirectiveHeader {
+        IgvmDirectiveHeader::ParameterInsert(IGVM_VHS_PARAMETER_INSERT {
+            gpa: page * PAGE_SIZE_4K,
+            parameter_area_index: index,
+            compatibility_mask: mask,
+        })
+    }
+
+    fn create_igvm() -> Vec<u8> {
+        let data1 = vec![1; PAGE_SIZE_4K as usize];
+        let data2 = vec![2; PAGE_SIZE_4K as usize];
+        let data3 = vec![3; PAGE_SIZE_4K as usize];
+        let data4 = vec![4; PAGE_SIZE_4K as usize];
+        let file = IgvmFile::new(
+            IgvmRevision::V1,
+            vec![new_platform(0x1, IgvmPlatformType::VSM_ISOLATION)],
+            vec![new_guest_policy(0x30000, 1), new_guest_policy(0x30000, 2)],
+            vec![
+                new_page_data(0, 1, &data1),
+                new_page_data(1, 1, &data2),
+                new_page_data(2, 1, &data3),
+                new_page_data(4, 1, &data4),
+                new_page_data(10, 1, &data1),
+                new_page_data(11, 1, &data2),
+                new_page_data(12, 1, &data3),
+                new_page_data(14, 1, &data4),
+                new_parameter_area(0),
+                new_parameter_usage(0),
+                new_parameter_insert(20, 0, 1),
+            ],
+        )
+        .expect("Failed to create file");
+        let mut binary_file = Vec::new();
+        file.serialize(&mut binary_file).unwrap();
+        binary_file
+    }
+
+    #[test]
+    fn c_api_parse_valid_file() {
+        let file_data = create_igvm();
+        // SAFETY: The function call requires the size of the buffer to be at least as
+        // large as the specified length. We are passing a pointer to a vector along with
+        // the length of the vector so the safety requirements are met.
+        let igvm = unsafe { igvm_new_from_binary(file_data.as_ptr(), file_data.len() as u32) };
+        assert!(igvm > 0);
+        igvm_free(igvm);
+    }
+
+    #[test]
+    fn c_api_test_invalid_fixed_header() {
+        let invalid_igvm_buf: [u8; 16] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+        // SAFETY: The function call requires the size of the buffer to be at least as
+        // large as the specified length. We are passing a pointer to a fixed array along with
+        // the length of the same fixed array so the safety requirements are met.
+        let igvm = unsafe {
+            igvm_new_from_binary(invalid_igvm_buf.as_ptr(), invalid_igvm_buf.len() as u32)
+        };
+        assert_eq!(igvm, IgvmResult::IGVMAPI_INVALID_FIXED_HEADER.0);
+    }
+
+    #[test]
+    fn c_api_test_header_counts() {
+        let file_data = create_igvm();
+        // SAFETY: The function call requires the size of the buffer to be at least as
+        // large as the specified length. We are passing a pointer to a vector along with
+        // the length of the vector so the safety requirements are met.
+        let igvm = unsafe { igvm_new_from_binary(file_data.as_ptr(), file_data.len() as u32) };
+        assert_eq!(
+            igvm_header_count(igvm, IgvmHeaderSection::HEADER_SECTION_PLATFORM),
+            1
+        );
+        assert_eq!(
+            igvm_header_count(igvm, IgvmHeaderSection::HEADER_SECTION_INITIALIZATION),
+            2
+        );
+        assert_eq!(
+            igvm_header_count(igvm, IgvmHeaderSection::HEADER_SECTION_DIRECTIVE),
+            11
+        );
+        igvm_free(igvm);
+    }
+
+    #[test]
+    fn c_api_test_platform_header() {
+        let file_data = create_igvm();
+        // SAFETY: The function call requires the size of the buffer to be at least as
+        // large as the specified length. We are passing a pointer to a vector along with
+        // the length of the vector so the safety requirements are met.
+        let igvm = unsafe { igvm_new_from_binary(file_data.as_ptr(), file_data.len() as u32) };
+
+        let data = igvm_get_header(igvm, IgvmHeaderSection::HEADER_SECTION_PLATFORM, 0);
+        assert!(data > 0);
+
+        // SAFETY: igvm_get_buffer returns a raw pointer that can only be used up until
+        // igvm_free_buffer() is called. This is satisfied by the code below. In addition, the
+        // returned pointer is safely cast to the required type only after checking the
+        // type reported in the header.
+        unsafe {
+            let header = igvm_get_buffer(igvm, data) as *const IGVM_VHS_VARIABLE_HEADER;
+            assert_eq!(
+                (*header).typ,
+                IgvmVariableHeaderType::IGVM_VHT_SUPPORTED_PLATFORM
+            );
+            assert_eq!(
+                (*header).length,
+                size_of::<IGVM_VHS_SUPPORTED_PLATFORM>() as u32
+            );
+
+            let platform = header.add(1) as *const IGVM_VHS_SUPPORTED_PLATFORM;
+            assert_eq!((*platform).platform_type, IgvmPlatformType::VSM_ISOLATION);
+            assert_eq!((*platform).compatibility_mask, 1);
+            assert_eq!((*platform).platform_version, 1);
+            assert_eq!((*platform).highest_vtl, 0);
+            assert_eq!((*platform).shared_gpa_boundary, 0);
+        }
+        igvm_free_buffer(igvm, data);
+
+        igvm_free(igvm);
+    }
+
+    #[test]
+    fn c_api_test_initialization_header() {
+        let file_data = create_igvm();
+        // SAFETY: The function call requires the size of the buffer to be at least as
+        // large as the specified length. We are passing a pointer to a vector along with
+        // the length of the vector so the safety requirements are met.
+        let igvm = unsafe { igvm_new_from_binary(file_data.as_ptr(), file_data.len() as u32) };
+
+        let data = igvm_get_header(igvm, IgvmHeaderSection::HEADER_SECTION_INITIALIZATION, 0);
+        assert!(data > 0);
+        // SAFETY: igvm_get_buffer returns a raw pointer that can only be used up until
+        // igvm_free_buffer() is called. This is satisfied by the code below. In addition, the
+        // returned pointer is safely cast to the required type only after checking the
+        // type reported in the header.
+        unsafe {
+            let header = igvm_get_buffer(igvm, data) as *const IGVM_VHS_VARIABLE_HEADER;
+            assert_eq!((*header).typ, IgvmVariableHeaderType::IGVM_VHT_GUEST_POLICY);
+            assert_eq!((*header).length, size_of::<IGVM_VHS_GUEST_POLICY>() as u32);
+
+            let policy = header.add(1) as *const IGVM_VHS_GUEST_POLICY;
+            assert_eq!((*policy).policy, 0x30000);
+            assert_eq!((*policy).compatibility_mask, 1);
+            assert_eq!((*policy).reserved, 0);
+        }
+        igvm_free_buffer(igvm, data);
+
+        let data = igvm_get_header(igvm, IgvmHeaderSection::HEADER_SECTION_INITIALIZATION, 1);
+        assert!(data > 0);
+        // SAFETY: igvm_get_buffer returns a raw pointer that can only be used up until
+        // igvm_free_buffer() is called. This is satisfied by the code below. In addition, the
+        // returned pointer is safely cast to the required type only after checking the
+        // type reported in the header.
+        unsafe {
+            let header = igvm_get_buffer(igvm, data) as *const IGVM_VHS_VARIABLE_HEADER;
+            assert_eq!((*header).typ, IgvmVariableHeaderType::IGVM_VHT_GUEST_POLICY);
+            assert_eq!((*header).length, size_of::<IGVM_VHS_GUEST_POLICY>() as u32);
+
+            let policy = header.add(1) as *const IGVM_VHS_GUEST_POLICY;
+            assert_eq!((*policy).policy, 0x30000);
+            assert_eq!((*policy).compatibility_mask, 2);
+            assert_eq!((*policy).reserved, 0);
+        }
+        igvm_free_buffer(igvm, data);
+
+        igvm_free(igvm);
+    }
+
+    #[test]
+    fn c_api_test_directive_header() {
+        let file_data = create_igvm();
+        // SAFETY: The function call requires the size of the buffer to be at least as
+        // large as the specified length. We are passing a pointer to a vector along with
+        // the length of the vector so the safety requirements are met.
+        let igvm = unsafe { igvm_new_from_binary(file_data.as_ptr(), file_data.len() as u32) };
+
+        let data = igvm_get_header(igvm, IgvmHeaderSection::HEADER_SECTION_DIRECTIVE, 1);
+        assert!(data > 0);
+        // SAFETY: igvm_get_buffer returns a raw pointer that can only be used up until
+        // igvm_free_buffer() is called. This is satisfied by the code below. In addition, the
+        // returned pointer is safely cast to the required type only after checking the
+        // type reported in the header.
+        unsafe {
+            let header = igvm_get_buffer(igvm, data) as *const IGVM_VHS_VARIABLE_HEADER;
+            assert_eq!((*header).typ, IgvmVariableHeaderType::IGVM_VHT_PAGE_DATA);
+            assert_eq!((*header).length, size_of::<IGVM_VHS_PAGE_DATA>() as u32);
+
+            let page = header.add(1) as *const IGVM_VHS_PAGE_DATA;
+            assert_eq!((*page).data_type, IgvmPageDataType::NORMAL);
+            assert_eq!((*page).compatibility_mask, 1);
+            assert_eq!((*page).file_offset, 0);
+            assert_eq!((*page).flags.is_2mb_page(), false);
+            assert_eq!((*page).flags.unmeasured(), false);
+            assert_eq!((*page).flags.reserved(), 0);
+            assert_eq!((*page).gpa, 0x1000);
+            assert_eq!((*page).reserved, 0);
+        }
+        igvm_free_buffer(igvm, data);
+
+        let data = igvm_get_header(igvm, IgvmHeaderSection::HEADER_SECTION_DIRECTIVE, 8);
+        assert!(data > 0);
+        // SAFETY: igvm_get_buffer returns a raw pointer that can only be used up until
+        // igvm_free_buffer() is called. This is satisfied by the code below. In addition, the
+        // returned pointer is safely cast to the required type only after checking the
+        // type reported in the header.
+        unsafe {
+            let header = igvm_get_buffer(igvm, data) as *const IGVM_VHS_VARIABLE_HEADER;
+            assert_eq!(
+                (*header).typ,
+                IgvmVariableHeaderType::IGVM_VHT_PARAMETER_AREA
+            );
+            assert_eq!(
+                (*header).length,
+                size_of::<IGVM_VHS_PARAMETER_AREA>() as u32
+            );
+
+            let param_area = header.add(1) as *const IGVM_VHS_PARAMETER_AREA;
+            assert_eq!((*param_area).parameter_area_index, 0);
+            assert_eq!((*param_area).file_offset, 0);
+            assert_eq!((*param_area).number_of_bytes, 0x1000);
+        }
+        igvm_free_buffer(igvm, data);
+
+        let data = igvm_get_header(igvm, IgvmHeaderSection::HEADER_SECTION_DIRECTIVE, 9);
+        assert!(data > 0);
+        // SAFETY: igvm_get_buffer returns a raw pointer that can only be used up until
+        // igvm_free_buffer() is called. This is satisfied by the code below. In addition, the
+        // returned pointer is safely cast to the required type only after checking the
+        // type reported in the header.
+        unsafe {
+            let header = igvm_get_buffer(igvm, data) as *const IGVM_VHS_VARIABLE_HEADER;
+            assert_eq!(
+                (*header).typ,
+                IgvmVariableHeaderType::IGVM_VHT_VP_COUNT_PARAMETER
+            );
+            assert_eq!((*header).length, size_of::<IGVM_VHS_PARAMETER>() as u32);
+
+            let param = header.add(1) as *const IGVM_VHS_PARAMETER;
+            assert_eq!((*param).parameter_area_index, 0);
+            assert_eq!((*param).byte_offset, 0);
+        }
+        igvm_free_buffer(igvm, data);
+
+        let data = igvm_get_header(igvm, IgvmHeaderSection::HEADER_SECTION_DIRECTIVE, 10);
+        assert!(data > 0);
+        // SAFETY: igvm_get_buffer returns a raw pointer that can only be used up until
+        // igvm_free_buffer() is called. This is satisfied by the code below. In addition, the
+        // returned pointer is safely cast to the required type only after checking the
+        // type reported in the header.
+        unsafe {
+            let header = igvm_get_buffer(igvm, data) as *const IGVM_VHS_VARIABLE_HEADER;
+            assert_eq!(
+                (*header).typ,
+                IgvmVariableHeaderType::IGVM_VHT_PARAMETER_INSERT
+            );
+            assert_eq!(
+                (*header).length,
+                size_of::<IGVM_VHS_PARAMETER_INSERT>() as u32
+            );
+
+            let ins = header.add(1) as *const IGVM_VHS_PARAMETER_INSERT;
+            assert_eq!((*ins).parameter_area_index, 0);
+            assert_eq!((*ins).compatibility_mask, 1);
+            assert_eq!((*ins).gpa, 0x14000);
+        }
+        igvm_free_buffer(igvm, data);
+
+        igvm_free(igvm);
+    }
+
+    #[test]
+    fn c_api_test_associated_data() {
+        let file_data = create_igvm();
+        // SAFETY: The function call requires the size of the buffer to be at least as
+        // large as the specified length. We are passing a pointer to a vector along with
+        // the length of the vector so the safety requirements are met.
+        let igvm = unsafe { igvm_new_from_binary(file_data.as_ptr(), file_data.len() as u32) };
+
+        let data = igvm_get_header_data(igvm, IgvmHeaderSection::HEADER_SECTION_DIRECTIVE, 3);
+        assert!(data > 0);
+        let data_length = igvm_get_buffer_size(igvm, data);
+        assert_eq!(data_length, 0x1000);
+        // SAFETY: igvm_get_buffer returns a raw pointer that can only be used up until
+        // igvm_free_buffer() is called. This is satisfied by the code below. In addition, the
+        // returned pointer is safely cast to the required type only after checking the
+        // type reported in the header.
+        unsafe {
+            let buf = igvm_get_buffer(igvm, data);
+            for i in 0..data_length as usize {
+                assert_eq!(buf.add(i).read(), 4);
+            }
+        }
+        igvm_free_buffer(igvm, data);
+
+        igvm_free(igvm);
+    }
+
+    #[test]
+    fn c_api_test_no_associated_data() {
+        let file_data = create_igvm();
+        // SAFETY: The function call requires the size of the buffer to be at least as
+        // large as the specified length. We are passing a pointer to a vector along with
+        // the length of the vector so the safety requirements are met.
+        let igvm = unsafe { igvm_new_from_binary(file_data.as_ptr(), file_data.len() as u32) };
+
+        let data = igvm_get_header_data(igvm, IgvmHeaderSection::HEADER_SECTION_DIRECTIVE, 9);
+        assert!(data < 0);
+        assert_eq!(data, IgvmResult::IGVMAPI_NO_DATA.0);
+
+        igvm_free(igvm);
+    }
+}
